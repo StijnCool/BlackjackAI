fd4d21a50e3da6d839b7c79a4a18e33b5c451d7a
Stijn Hennissen <hennissenstijn@gmail.com>
2022-06-09 23:58:43 +0200

    Done

--- src/Game.java
+++ src/Game.java
@@ -1,36 +1,120 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Game {
     private int maxScore;
     private int currentScore;
     private List<Integer> currentDeck;
     private List<Integer> cardsDrawn;
+    private List<Integer> originalDeck;
     private boolean rememberDeck;
+
+    private Random random = new Random();
 
     public Game(){
         maxScore = 21;
         currentScore = 0;
         currentDeck = createStandardDeck(1);
         cardsDrawn = new ArrayList<>();
+        originalDeck = currentDeck;
         rememberDeck = true;
+    }
+
+    public Game(int _maxScore, int numDecks, boolean _rememberDeck){
+        maxScore = _maxScore;
+        currentScore = 0;
+        currentDeck = createStandardDeck(numDecks);
+        cardsDrawn = new ArrayList<>();
+        originalDeck = currentDeck;
+        rememberDeck = _rememberDeck;
+    }
+
+    public Game(boolean _rememberDeck){
+        maxScore = 21;
+        currentScore = 0;
+        currentDeck = createStandardDeck(1);
+        cardsDrawn = new ArrayList<>();
+        originalDeck = currentDeck;
+        rememberDeck = _rememberDeck;
+    }
+
+    public Game(Game game){
+        maxScore = game.getMaxScore();
+        currentScore = game.getCurrentScore();
+        currentDeck = new ArrayList<>(game.getCurrentDeck());
+        cardsDrawn = new ArrayList<>(game.getCardsDrawn());
+        originalDeck = new ArrayList<>(game.getOriginalDeck());
+        rememberDeck = game.isRememberDeck();
+    }
+    public Game(int _maxScore, int _currentScore, List<Integer> _currentDeck, List<Integer> _cardsDrawn, List<Integer> _originalDeck, boolean _rememberDeck){
+        maxScore = _maxScore;
+        currentScore = _currentScore;
+        currentDeck = _currentDeck;
+        cardsDrawn = _cardsDrawn;
+        originalDeck = _originalDeck;
+        rememberDeck = _rememberDeck;
     }
 
     private List<Integer> createStandardDeck(int numDecks) {
         List<Integer> deck = new ArrayList<>();
         for(int i=1;i<=numDecks;i++){
-            for(int j=1;j<=4;j++){
-                for(int k=2;k<=11;k++){
-                    deck.add(k);
-                    if(k==10){
-                        deck.add(k);
-                        deck.add(k);
-                        deck.add(k);
+            for(int j=2;j<=11;j++){
+                for(int k=1;k<=4;k++){
+                    deck.add(j);
+                    if(j==10){
+                        deck.add(j);
+                        deck.add(j);
+                        deck.add(j);
                     }
                 }
             }
         }
         return deck;
+    }
+
+    public int drawRandomCard(boolean isSelf){
+        int id = random.nextInt(currentDeck.size());
+        int card = currentDeck.get(id);
+        if(card==11 && isSelf && currentScore+card>maxScore){
+            card = 1;
+        }
+        currentDeck.remove(id);
+        if(isSelf){
+            cardsDrawn.add(card);
+            currentScore += card;
+        }
+        return card;
+    }
+
+    public int drawCard(int card, boolean isSelf){
+        if(!currentDeck.contains(card)){return -1;}
+        int id = currentDeck.indexOf(card);
+        if(card==11 && isSelf && currentScore+card>maxScore){
+            card = 1;
+        }
+        currentDeck.remove(id);
+        if(isSelf){
+            cardsDrawn.add(card);
+            currentScore += card;
+        }
+        return card;
+    }
+
+    public double calculateSafeChance(){
+        double chance = 0;
+        if(rememberDeck){
+            for(int card : currentDeck){if(currentScore+card<=maxScore){chance++;}}
+            chance /= currentDeck.size();
+        }else{
+            for(int card : originalDeck){if(currentScore+card<=maxScore){chance++;}}
+            chance /= originalDeck.size();
+        }
+        return chance;
+    }
+
+    public boolean isValidScore(){
+        return currentScore <= maxScore;
     }
 
     public int getMaxScore() {
@@ -51,6 +135,10 @@ public class Game {
 
     public boolean isRememberDeck() {
         return rememberDeck;
+    }
+
+    public List<Integer> getOriginalDeck() {
+        return originalDeck;
     }
 
     @Override
--- src/main.java
+++ src/main.java
@@ -1,6 +1,92 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
-        Game game = new Game();
-        System.out.println(game.toString());
+        Scanner S = new Scanner(System.in);
+        System.out.println("Welcome to the Blackjack AI");
+        System.out.println("What is the maximum score allowed?");
+        int maxScore = S.nextInt();
+        System.out.println("How many decks of cards will be used?");
+        int numDecks = S.nextInt();
+        System.out.println("Start of the game!");
+        Game game = new Game(maxScore,numDecks,true);
+        while(game.isValidScore()){
+            System.out.println("When you draw another card you have a "+game.calculateSafeChance()*100+"% chance of not going over "+game.getMaxScore()+".");
+            System.out.println("1) Draw own card");
+            System.out.println("2) Draw other card");
+            int person = S.nextInt();
+            System.out.println("Card value? Enter 1 for a random card.");
+            int card = S.nextInt();
+            if(card==1){
+                System.out.println("A "+game.drawRandomCard(person==1)+" was drawn.");
+            }else {
+                game.drawCard(card, person==1);
+            }
+            if(person==1){System.out.println("Your score is now " + game.getCurrentScore() + "/" + game.getMaxScore() + ".");}
+        }
+    }
+
+    public static void doTest(){
+        List<List<Double>> rememberTrueData = new ArrayList<>();
+        List<List<Double>> rememberFalseData = new ArrayList<>();
+
+        // Run simulations
+        Game game = new Game(false);
+        for(double t=1;t>=0;t=round(t-0.1,1)) {
+            List<Double> runData = new ArrayList<>();
+            runData.add(t);
+            runData.addAll(runGames(game, 1000, false, t));
+            rememberFalseData.add(runData);
+            System.out.println(runData);
+        }
+        System.out.println();
+        game = new Game(true);
+        for(double t=1;t>=0;t=round(t-0.1,1)) {
+            List<Double> runData = new ArrayList<>();
+            runData.add(t);
+            runData.addAll(runGames(game, 1000, false, t));
+            rememberTrueData.add(runData);
+            System.out.println(runData);
+        }
+    }
+
+    public static List<Double> runGames(Game baseGame, int amount, boolean doPrint, double threshold){
+        double losses = 0;
+        double wins = 0;
+        List<Double> finalScores = new ArrayList<>();
+        for(int n=1;n<=amount;n++){
+            Game game = new Game(baseGame);
+            if(doPrint){System.out.println(game.toString());};
+            while(game.isValidScore() && game.calculateSafeChance()>threshold){
+                if(doPrint) {
+                    System.out.println(game.calculateSafeChance() * 100 + "% chance to not lose");
+                    System.out.println("You draw a " + game.drawRandomCard(true));
+                    System.out.println(game.toString());
+                }else{
+                    game.drawRandomCard(true);
+                }
+            }
+            if(game.isValidScore()){
+                wins++;
+                if(doPrint){System.out.println("You did not lose!");}
+            }else{
+                losses++;
+                if(doPrint){System.out.println("You lost!");}
+            }
+            finalScores.add((double) game.getCurrentScore());
+        }
+        // System.out.println("wins="+wins/amount*100+"%, losses="+losses/amount*100+"%");
+        List<Double> R = new ArrayList<>();
+        R.add(wins/amount);
+        R.add(losses/amount);
+        R.addAll(finalScores);
+        return R;
+    }
+
+    private static double round (double value, int precision) {
+        int scale = (int) Math.pow(10, precision);
+        return (double) Math.round(value * scale) / scale;
     }
 }
