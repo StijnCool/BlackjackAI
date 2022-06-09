c202c347318b739c8fd00788104978c14763927f
Stijn Hennissen <hennissenstijn@gmail.com>
2022-06-09 13:37:02 +0200

    In Game.java filled Game.createStandardDeck and added getters

--- src/Game.java
+++ src/Game.java
@@ -11,12 +11,50 @@ public class Game {
     public Game(){
         maxScore = 21;
         currentScore = 0;
-        currentDeck = createStandardDeck();
+        currentDeck = createStandardDeck(1);
         cardsDrawn = new ArrayList<>();
         rememberDeck = true;
     }
 
-    private List<Integer> createStandardDeck() {
-        return new ArrayList<>();
+    private List<Integer> createStandardDeck(int numDecks) {
+        List<Integer> deck = new ArrayList<>();
+        for(int i=1;i<=numDecks;i++){
+            for(int j=1;j<=4;j++){
+                for(int k=2;k<=11;k++){
+                    deck.add(k);
+                    if(k==10){
+                        deck.add(k);
+                        deck.add(k);
+                        deck.add(k);
+                    }
+                }
+            }
+        }
+        return deck;
+    }
+
+    public int getMaxScore() {
+        return maxScore;
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public List<Integer> getCurrentDeck() {
+        return currentDeck;
+    }
+
+    public List<Integer> getCardsDrawn() {
+        return cardsDrawn;
+    }
+
+    public boolean isRememberDeck() {
+        return rememberDeck;
+    }
+
+    @Override
+    public String toString(){
+        return "Game{score=["+currentScore+"/"+maxScore+"], drawn="+cardsDrawn+", rememberDeck=["+rememberDeck+"]}";
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args){
-
+        Game game = new Game();
+        System.out.println(game.toString());
     }
 }
